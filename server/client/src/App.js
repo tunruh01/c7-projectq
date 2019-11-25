3e75df8663b15d5d028040f300f1734ee2ad255f
Alexandra Ferdinand <alexxkf@gmail.com>
2019-11-25 16:22:06 -0500

    added skeleton for questionlist and app components

---
+++ server/client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import QuestionList from './components/QuestionList'
+import CategoryList from './components/CategoryList'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <QuestionList />
+        <CategoryList />
+      </div>
+    )
+  }
+}
+
+export default App
