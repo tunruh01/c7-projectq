8c71117bca864fed947862ea08cce044f9fc4739
daveman235 <awad.david@gmail.com>
2019-11-25 15:56:07 -0500

    navbar component added

---
+++ server/client/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import { Link } from "react-router-dom";
+
+
+class NavBar extends Component {
+
+
+    render() {
+        <>
+            <Navbar bg="dark" variant="dark" fixed="top">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+            <br />
+            <Navbar bg="primary" variant="dark">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-light">Search</Button>
+                </Form>
+            </Navbar>
+
+            <br />
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-primary">Search</Button>
+                </Form>
+            </Navbar>
+        </>
+    }
+
+
+}
+
+export default NavBar;
