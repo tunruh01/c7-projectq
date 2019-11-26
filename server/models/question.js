d56516f68f70e890a74bc3e31daa36be60b981cc
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-26 11:17:06 -0500

    jen's question model

---
+++ server/models/question.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const QuestionsSchema = new Schema({
+  id: String,
+  topic: [{ type: Schema.Types.ObjectId, ref: "topic" }],
+  question: [{ type: String, ref: "question" }],
+  userId: [{ type: Schema.Types.ObjectId, ref: user }],
+  answers: [{ type: Schema.Types.ObjectId, ref: answerId }],
+  dateAdded: Date,
+  DateModified: Date
+});
+
+module.exports = mongoose.model("question", QuestionsSchema);
