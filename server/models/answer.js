7589243d5cae182b752c338041ae54f9c363ca17
Tyler <laorik1@live.com>
2019-11-26 11:20:06 -0500

    fixed schemas tha twere using arrays instead of single instances of ref object

--- server/models/answer.js
+++ server/models/answer.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AnswerSchema = new Schema({
   answer: String,
-  userId: [{type: mongoose.Schema.Types.ObjectId, ref: 'user'}],
-  questionId: [{type: mongoose.Schema.Types.ObjectId, ref: 'question'}],
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  questionId: { type: mongoose.Schema.Types.ObjectId, ref: "question" },
   score: Number,
   dateAdded: Date,
   dateModified: Date,
-  comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'comment'}]
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "comment" }]
 });
 
-module.exports = mongoose.model('answer', AnswerSchema);
+module.exports = mongoose.model("answer", AnswerSchema);
--- server/models/question.js
+++ server/models/question.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const QuestionsSchema = new Schema({
   id: String,
-  topic: [{ type: Schema.Types.ObjectId, ref: 'topic' }],
-  question: [{ type: String, ref: "question" }],
-  userId: [{ type: Schema.Types.ObjectId, ref: 'user' }],
-  answers: [{ type: Schema.Types.ObjectId, ref: 'answer' }],
+  topics: [{ type: Schema.Types.ObjectId, ref: "topic" }],
+  question: { type: String, ref: "question" },
+  userId: { type: Schema.Types.ObjectId, ref: "user" },
+  answers: [{ type: Schema.Types.ObjectId, ref: "answer" }],
   dateAdded: Date,
   DateModified: Date
 });
