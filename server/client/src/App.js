26fffa874787462406c00b906f49ba150577e81c
daveman235 <awad.david@gmail.com>
2019-11-26 10:33:44 -0500

    Navbar is rendering...finally

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
-import Navbar from './components/Navbar'
 import QuestionList from './components/QuestionList'
 import CategoryList from './components/CategoryList'
+import Qnav from "./components/Qnav";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <QuestionList />
-        <CategoryList />
+        <Qnav />
+        {/* <QuestionList />
+        <CategoryList /> */}
       </div>
     )
   }
--- server/client/src/components/Qnav.js
+++ server/client/src/components/Qnav.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Navbar from 'react-bootstrap/Navbar'
-import { Form, FormControl } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import Nav from "react-bootstrap/Nav";
 
 
 // Nav.Link under formcontrol was a button, can't tell if that Nav.link works yet
@@ -9,13 +12,14 @@ class Qnav extends Component {
     render() {
         return (
             <Navbar bg="light" variant="light">
-                <Navbar.Brand href="#home">ProjectQ</Navbar.Brand>
+                <Navbar.Brand href="/">ProjectQ</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-primary">Search</Button>
+                    <Button variant="outline-primary">Ask Q</Button>
                 </Form>
             </Navbar>
         )
