fb9d4f5e0e8c8dc7216d0af3755366d60110abe8
Tyler <laorik1@live.com>
2019-11-26 11:26:57 -0500

    also removing comment instance

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,17 +1,14 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
   id: String,
   comment: String,
-  userId: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+  userId: { type: Schema.Types.ObjectId, ref: "user" },
   score: Number,
   dateAdded: Date,
   dateModified: Date,
-  answer: [{ type: Schema.Types.ObjectId, ref: 'answer' }]
-})
+  answer: { type: Schema.Types.ObjectId, ref: "answer" }
+});
 
-
-module.exports = mongoose.model('comment', CommentSchema)
-
+module.exports = mongoose.model("comment", CommentSchema);
