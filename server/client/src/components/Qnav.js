5368d93b84552c3a75fbfaa72ba40f5666423b51
Alexandra Ferdinand <alexxkf@gmail.com>
2019-11-27 11:16:21 -0500

    stylin for navbar

--- server/client/src/components/Qnav.js
+++ server/client/src/components/Qnav.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar from 'react-bootstrap/Navbar'
-import { Form, FormControl, Button, Nav } from "react-bootstrap";
+import { Form, FormControl, Button, Nav, Col, Image } from "react-bootstrap";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 
@@ -11,15 +11,18 @@ class Qnav extends Component {
         return (
 
             <Router>
-                <Navbar bg="dark" variant="dark">
+                <Navbar bg="light" variant="light">
                     <Link exact to="/"><Navbar.Brand href="/">Project Q</Navbar.Brand></Link>
                     <Nav className="mr-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                     </Nav>
                     <Form inline>
-                        <FormControl type="text" placeholder="Search Q" className="mr-sm-2" />
-                        <Button variant="outline-primary">Search</Button>
-                        <Link exact to="/questions"><Button variant="outline-primary">Ask Q</Button></Link>
+                        <FormControl size="sm" type="text" placeholder="Search Questions" className="mr-md-2" />
+                        <Button variant="outline-secondary mr-1" size="sm">Search</Button>
+                        <Col xs={6} md={4}>
+                            <Image src="holder.js/171x180" roundedCircle />
+                        </Col>
+                        <Link exact to="/questions"><Button variant="outline-danger mr-1" size="sm">Ask Question</Button></Link>
                     </Form>
                 </Navbar>
             </Router>
