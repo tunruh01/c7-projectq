aa2e4c21b51e2b534b5a0dbbc83d72913d3792dd
Billy DeLashmutt <delashmuttwa@vt.edu>
2019-11-26 22:23:31 -0500

    Hardcoded 'response' implemented for questionList testing. Renders to the page on load.

--- server/client/src/components/QuestionList.js
+++ server/client/src/components/QuestionList.js
@@ -8,32 +8,32 @@ import { connect } from "react-redux";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class QuestionList extends Component {
-  constructor() {
-    super()
+  // constructor() {
+  //   super()
 
-    this.state = {
-      questions: [
-        {
-          id: 1,
-          category: "development",
-          name: "fred",
-          text: "will we be ok?"
-        },
-        {
-          id: 2,
-          category: "development",
-          name: "fred",
-          text: "will we be ok?"
-        },
-        {
-          id: 3,
-          category: "development",
-          name: "fred",
-          text: "will we be ok?"
-        }
-      ]
-    }
-  }
+    // this.state = {
+    //   questions: [
+    //     {
+    //       id: 1,
+    //       category: "development",
+    //       name: "fred",
+    //       text: "will we be ok?"
+    //     },
+    //     {
+    //       id: 2,
+    //       category: "development",
+    //       name: "fred",
+    //       text: "will we be ok?"
+    //     },
+    //     {
+    //       id: 3,
+    //       category: "development",
+    //       name: "fred",
+    //       text: "will we be ok?"
+    //     }
+    //   ]
+    // }
+  // }
   // Fetch questions once page assets are ready
   componentDidMount () {
     this.props.fetchQuestions()
@@ -44,29 +44,43 @@ class QuestionList extends Component {
   //   this.props.history.push(path);
   // }
 
-  //use Switch to ensure only one route renders at a time
-  renderQuestions() {
-    let questions = this.state.questions
+  // renderQuestions() {
+  //   let questions = this.state.questions
 
-    return (
-      questions.map(q => (
-        <p key={q.id}>
-          <a href='/' onClick={e => { e.preventDefault(this.fetchQuestions(q.id)); }}>{q.name}</a>
-        </p>
-      ))
+  //   return (
+  //     questions.map(q => (
+  //       <p key={q.id}>
+  //         <a href='/' onClick={e => { e.preventDefault(this.fetchQuestions(q.id)); }}>{q.name}</a>
+  //       </p>
+  //     ))
+  //   )
+  // }
+
+  renderQuestions() {
+    // If questions in state, loop and return each one
+    if(this.props.questions.questionsList) {
+      return (
+        this.props.questions.questionsList.map(q => (
+          <p key={parseInt(q.id)}>
+            <a href='/' onClick={e => { e.preventDefault(this.fetchQuestions(q.id)); }}>{q.question}</a>
+            <p>{q.topAnswer.answer}</p>
+          </p>
+        ))
     )
+    }
+    
   }
 
   
-
   render() {
+    console.log('questionList render props: ', this.props)
     return (
       <div>
       <CategoryList/>
         <div className="QuestionList">
           <div className="container">
             <h1>Questions</h1>
-            
+            {this.renderQuestions()}
           </div>
         </div>
       </div>
@@ -76,7 +90,7 @@ class QuestionList extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return {questions: state.questions}
+  return state
 }
 
 const mapDispatchToProps = (dispatch) => {
--- server/client/src/reducers/questionsReducer.js
+++ server/client/src/reducers/questionsReducer.js
@@ -1,13 +1,20 @@
 import { FETCH_QUESTIONS } from '../actions/actions';
 
+const DEFAULT_STATE = {
+  questionsList: []
+}
 
-export default function(state = {}, action) {
+export default function(state = DEFAULT_STATE, action) {
   switch (action.type) {
     case FETCH_QUESTIONS:
-      console.log(action.payload)
-
-      return {'asdf':'result of FETCH_QUESTIONS reducer'};
-
+      if (action.payload) {
+        let newState = Object.assign({}, state)
+        action.payload.questions.forEach(question => {
+          newState.questionsList.push(question)
+          })
+        return newState;
+      }
+      return state;
     default:
       return state;
   }
