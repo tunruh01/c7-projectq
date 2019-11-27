f383cb76247a06f792b9df7f12d79c945d8db505
Tyler <laorik1@live.com>
2019-11-27 12:43:16 -0500

    updating variables to link with heroku

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,6 @@
 // for dev
 module.exports = {
-  MONGODB_URI: "mongodb://localhost/projectq"
+  MONGODB_URI: "mongodb://localhost/projectq",
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  CLIENT_ID: process.env.CLIENT_ID
 };
