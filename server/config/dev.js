5d17fd5583ff564859b55830264e605393def2f9
Tyler <laorik1@live.com>
2019-11-27 14:01:25 -0500

    adding valid cookie keys

--- server/config/dev.js
+++ server/config/dev.js
@@ -2,5 +2,6 @@
 module.exports = {
   MONGODB_URI: "mongodb://localhost/projectq",
   CLIENT_SECRET: process.env.CLIENT_SECRET,
-  CLIENT_ID: process.env.CLIENT_ID
+  CLIENT_ID: process.env.CLIENT_ID,
+  COOKIE_KEY: process.env.COOKIE_KEY
 };
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
-  CLIENT_ID: process.env.CLIENT_ID
+  CLIENT_ID: process.env.CLIENT_ID,
+  COOKIE_KEY: process.env.COOKIE_KEY
 };
