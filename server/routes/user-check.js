e08ec826f6e86bc49a8d656afc96908c21d9d424
Tyler <laorik1@live.com>
2019-11-29 16:38:45 -0500

    turning on user response

--- server/routes/user-check.js
+++ server/routes/user-check.js
@@ -1,12 +1,12 @@
 const ensureAuthenticated = (req, res, next) => {
-  // if (!req.user) {
-  //   res.status(401).json({
-  //     authenticated: false,
-  //     message: "user has not been authenticated"
-  //   });
-  // } else {
-  next();
-  // }
+  if (!req.user) {
+    res.status(401).json({
+      authenticated: false,
+      message: "user has not been authenticated"
+    });
+  } else {
+    next();
+  }
 };
 
 module.exports = { UserAuthCheck: ensureAuthenticated };
