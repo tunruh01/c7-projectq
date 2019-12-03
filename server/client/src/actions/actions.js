1b3f2c1e5eee3def450ce0846ecffc843a42e0f8
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-03 11:05:27 -0500

    remove debugger

--- server/client/src/actions/actions.js
+++ server/client/src/actions/actions.js
@@ -87,7 +87,6 @@ export function upvoteAnswer(answerid) {
   };
 }
 export function createQuestion(values, callback) {
-  debugger;
   const request = axios.post(`${ROOT_URL}/question`, values, {
     withCredentials: true
   });
