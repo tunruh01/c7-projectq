2b7fe674d44682c7ff7b85dd0486b419486794c3
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-03 15:54:12 -0500

    Submit question form looks less garbage-esque

--- server/client/src/components/SubmitQuestion.js
+++ server/client/src/components/SubmitQuestion.js
@@ -43,42 +43,28 @@ class SubmitQuestion extends Component {
     return (
       <div className="row example-wrapper">
         <div className="col-xs-12 col-sm-6 offset-sm-3 example-col">
-          <div className="card">
-            <div className="card-block">
+          <div className="card submit-form-card mx-auto">
               <form className="k-form" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <div className='form-group'>
                   <legend>Ask Question Below</legend>
                   <label className="k-form-field">
-                    <span>Question:</span>
                     <Field
                     name="question"
                     placeholder="Start your question with What, How, Why, etc"
-                    component={'input'}
+                    component={'textarea'}
                     />                  
                   </label>
-                  {/* <div>
-                    {this.renderTopicChecklist(this.props.category.topics)}
-                  </div> */}
                   <Field
                   name='topics'
                   component={CheckboxGroup}
                   options={this.props.category.topics}
                   />
-                  {/* <div className="k-form-field">
-                    <span> Choose Categories: </span>
-
-                    <input type="radio" name="topics" id="latin" className="k-radio" />
-                    <label component={this.renderField} className="k-radio-label mr-2" for="latin"> Latin </label>
-
-                    <input type="radio" name="topics" id="languages" className="k-radio" checked="checked" />
-                    <label component={this.renderField} className="k-radio-label mr-2" for="languages"> Languages </label>
-                  </div> */}
                   <div className="text-right">
                     <a href='/'><Button variant="outline-danger mr-2" size="sm">Cancel</Button></a>
                     <Button type="submit" variant="outline-secondary mr-2" size="sm">Submit</Button>
                   </div>
-                   
+                </div>
               </form>
-            </div>
           </div>
         </div>
       </div>
